903728fb73b6c980345966f11e55c6799157b6fc
Peter Becich <peterbecich@gmail.com>
2016-09-06 17:39:43 -0700

    feature/DS12-409 lecture 4 tutorial

---
+++ tutorial/src/main/scala/com/datascience/education/tutorial/lecture4/SafeDivision.scala
@@ -0,0 +1,78 @@
+package com.datascience.education.tutorial.lecture4
+
+
+import scala.Option
+import scala.Some
+import scala.None
+
+import cats.Applicative
+import cats.Traverse
+import cats.instances.list._
+
+
+object SafeDivision {
+
+  // Task (2a)
+  // answer
+  def safeDiv(x: Int, y: Int): Option[Double] =
+    ???
+
+  def divTuple(tup: (Int, Int)): Option[Double] =
+    safeDiv(tup._1, tup._2)
+
+
+  import TraverseOption._
+  
+  // Task (2b)
+  def traverseFractions(ll: List[(Int, Int)]): Option[List[Double]] = 
+    ???
+
+
+}
+
+
+object SafeDivisionExamples extends App {
+
+  import SafeDivision._
+
+
+  println("Divide 7 by 2")
+  println(divTuple((7,2)))
+
+  println("Divide 7 by 0")
+  println(divTuple((7,0)))
+
+
+}
+
+object SafeDivisionTraversalExamples extends App {
+  import SafeDivision._
+
+
+  val a = (6 to 11).toList
+  val b = (-3 to 2).toList
+  val fracsFailing: List[Tuple2[Int, Int]] = a.zip(b)
+
+  val optionDoubles1: Option[List[Double]] =
+    traverseFractions(fracsFailing)
+
+  println("Option[List[Double]] in one step, using `traverse`. ")
+  println("should fail")
+  println(optionDoubles1)
+
+
+  println("-----------------")
+  val c = (6 to 11).toList
+  val d = (2 to 7).toList
+  val fracsSuccessful: List[Tuple2[Int, Int]] = c.zip(d)
+
+  println("These fractions do not include an undefined number")
+  val optionList2: Option[List[Double]] =
+    traverseFractions(fracsSuccessful)
+
+  println("Option[List[Double]] in one step, using `traverse`: ")
+  println(optionList2)
+
+}
+
+
