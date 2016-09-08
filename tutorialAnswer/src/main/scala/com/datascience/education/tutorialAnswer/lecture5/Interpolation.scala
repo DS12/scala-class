f6cee9f971dcc0a09dd61f08bd877d56a8d38355
Peter Becich <peterbecich@gmail.com>
2016-09-08 13:40:47 -0700

    feature/DS12-410 lecture 5 tutorial answers and exercises

---
+++ tutorialAnswer/src/main/scala/com/datascience/education/tutorialAnswer/lecture5/Interpolation.scala
@@ -0,0 +1,61 @@
+package com.datascience.education.tutorialAnswer.lecture5
+
+
+import com.datascience.education.common.lecture5.Stream
+import com.datascience.education.common.lecture5.Empty
+import com.datascience.education.common.lecture5.Cons
+import com.datascience.education.common.lecture5.Stream._
+
+object Sine {
+
+  def sinePositive: Stream[Double] =
+    Stream.cons(0,
+      Stream.cons(1.0/2,
+        Stream.cons(math.sqrt(3)/2,
+          Stream.cons(1.0,
+            Stream.cons(math.sqrt(3)/2,
+              Stream.cons(1.0/2, sineNegative)
+            )
+          )
+        )
+      )
+    )
+
+  def sineNegative: Stream[Double] =
+    sinePositive.map { d => -1*d }
+
+
+  val sine = sinePositive
+
+
+}
+
+
+
+
+object Stepper {
+  import Sine._
+
+  val stepperSine = sine.flatMap { d =>
+    Stream.cons(d, Stream.cons(d, Stream.empty))
+  }
+
+
+}
+
+
+
+object Interpolation {
+
+  import Sine._
+
+  // Task 1a
+  // Answer 1a
+  val linearInterpolated =
+    sine.zip(sine.tail).flatMap { case ((first: Double, second: Double)) =>
+      val vMid = (first+second)/2.0
+      Stream.cons(first, Stream.cons(vMid, Stream.empty))
+    }
+
+
+}
