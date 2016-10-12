254ea26d43840a0c6fb5f11670cd51a154e7a2a8
Hongfei Tian <bzsy911@gmail.com>
2016-10-12 16:43:46 -0700

    added answers to lecture3

---
+++ tutorialAnswer/src/main/scala/com/datascience/education/tutorialAnswer/lecture3/AsynchronousFactorials.scala
@@ -0,0 +1,111 @@
+package tutorialAnswers.lecture11
+
+import scala.concurrent._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object AsynchronousFactorial {
+
+  def factorial(n: Int): Int = {
+    val fact = if(n == 0) 1 else n*factorial(n-1)
+
+    Thread.sleep(500)
+    println(s"factorial($n) = $fact")
+
+    fact
+  }
+
+  def factorialAsync(n: Int): Future[Int] = Future(factorial(n))
+
+  def printFactorial(n: Int): Future[Int] = {
+    val fut: Future[Int] = factorialAsync(n)
+
+    fut.onSuccess {
+      case f => println(s"factorial $n is $f")
+    }
+
+    fut
+  }
+
+}
+
+object AsynchronousFactorialsExample extends App {
+
+  import AsynchronousFactorial._
+
+  val fut10 = printFactorial(10)
+  val fut20 = printFactorial(20)
+
+  (1 to 30).foreach(i => {Thread.sleep(500); println(s"unrelated: $i")})
+
+}
+
+import cats.syntax.applicative._
+import cats.syntax.writer._
+import cats.data.Writer
+
+object FactorialWriter {
+
+  // Task (1a)
+  import cats.std.vector._
+  type Logged[A] = Writer[Vector[String], A]
+
+  // Task (1b)
+  def factorial(n: Int): Logged[Int] = {
+    // val fact = if(n == 0) 1 else n*factorial(n-1)
+
+    val fact: Logged[Int] =
+      if(n == 0)
+        1.writer(Vector("hit bottom"))
+      else
+        factorial(n-1).flatMap( f =>
+          (n*f).writer(Vector(s"factorial($n) = ${n*f}"))
+        )
+
+    Thread.sleep(500)
+    //Writer(Vector(s"factorial($n) = $fact"), fact)
+
+    fact
+  }
+
+  // Task 1c
+  def factorialAsync(n: Int): Future[Logged[Int]] =
+    Future(factorial(n))
+
+}
+
+object FactorialWriterExample extends App {
+  import FactorialWriter._
+
+  // Task 1b
+  val fact10 = factorial(10)
+
+  val out = fact10.run
+
+  println(out)
+
+}
+
+object FactorialWriterAsyncExample extends App {
+  import FactorialWriter._
+
+
+  // Task 1d
+  // use transformer here, later
+  val futureFact10 = factorialAsync(10)
+  val futureFact12 = factorialAsync(12)
+
+  val fact10 = futureFact10.map(write => write.run)
+  val fact12 = futureFact12.map(write => write.run)
+
+  futureFact10.onSuccess {
+    case tup => println(tup)
+  }
+
+  futureFact12.onSuccess {
+    case tup => println(tup)
+  }
+
+
+}
+
---
+++ tutorialAnswer/src/main/scala/com/datascience/education/tutorialAnswer/lecture3/Database.scala
@@ -0,0 +1,138 @@
+package tutorialAnswers.lecture11
+
+import cats.data.Reader
+import cats.syntax.applicative._
+
+import cats.MonadReader
+
+import scala.collection.mutable.{Set => MutableSet}
+import scala.collection.mutable.{HashSet => MutableHashSet}
+import scala.collection.mutable.{Map => MutableMap}
+import scala.collection.mutable.HashMap
+
+import org.mindrot.jbcrypt.BCrypt
+
+import scala.util.Random
+/*
+ Note that in Reader, the `S` is static
+ In `State`, `S` is intended to change.
+ So this example does not "pass along" new copies of an immutable database
+ */
+
+sealed trait Database {
+
+  val users: MutableMap[Int, String]
+  val passwords: MutableMap[String, (String,String)]
+
+  def genId: Int
+
+  override def toString: String = {
+    val userString = users.toList.map(_.toString + '\n').mkString
+    val pwString = passwords.toList.map(_.toString + '\n').mkString
+
+    "Users: "+userString+" passwords: "+pwString
+
+  }
+}
+
+case class TestDatabase() extends Database {
+
+  val users: MutableMap[Int, String] = MutableMap.empty[Int,String]
+
+  // user name, user password hash
+  // http://www.mindrot.org/projects/jBCrypt/
+
+  // username, (salt, hash)
+  // val passwords: MutableMap[String, (String, String)] =
+  val passwords = MutableMap.empty[String,(String, String)]
+
+  val rand = new Random()
+
+  // unsafe if all ints used...
+  @annotation.tailrec
+  final def genId: Int = {
+    val r = rand.nextInt().abs
+    if (users.contains(r))
+      genId
+    else
+      r
+  }
+
+
+}
+
+
+object DatabaseQueriesAndUpdates {
+
+
+  // Task 2a
+  type DatabaseReader[A] = Reader[Database, A]
+
+  // Task 2b
+  def findUsername(userId: Int): DatabaseReader[Option[String]] =
+    Reader { (db: Database) =>
+      db.users.get(userId)
+    }
+
+  // Task 2c
+  def findUserId(username: String): DatabaseReader[Option[Int]] =
+    Reader { (db: Database) =>
+      db.users.find{case (userId: Int, usern: String) =>
+        usern == username}.map(_._1)
+    }
+
+  // Task 2d
+  def userExists(username: String): DatabaseReader[Boolean] =
+    findUserId(username).map(option => option.isDefined)
+
+  // Task 2e
+  def checkPassword(username: String, passwordClear: String): DatabaseReader[Boolean] =
+    Reader { (db: Database) =>
+      db.passwords.get(username).filter { case (salt: String, hash: String) =>
+        // val hashedCandidate = BCrypt.hashpw(passwordClear, salt)
+        BCrypt.checkpw(passwordClear, hash)
+      }.isDefined
+    }
+
+  // Task 2f
+  def checkLogin(userId: Int, passwordClear: String): DatabaseReader[Boolean] =
+    findUsername(userId).flatMap { opUsername =>
+
+      opUsername match {
+        case Some(username) => checkPassword(username, passwordClear)
+        case _ => false.pure[DatabaseReader]
+      }
+    }
+
+
+  // Task 2g
+
+  def createUser(username: String, passwordClear: String):
+      DatabaseReader[Option[Int]] =
+    userExists(username).flatMap { (exists: Boolean) =>
+      // println(s"create user $username $passwordClear exists $exists")
+      if(exists) Reader ( _ => None)
+      else Reader { (db: Database) => 
+        val id = db.genId
+        val salt: String = BCrypt.gensalt()
+        val passwordHashed = BCrypt.hashpw(passwordClear, salt)
+
+        db.users.update(id, username)
+        db.passwords.update(username, (salt, passwordHashed))
+
+        // println(db)
+
+        Some(id)
+      }
+    }
+
+}
+
+
+object DatabaseExample extends App {
+
+
+
+
+}
+
